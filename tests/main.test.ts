a6f7c4646c5e245e14ec8e4d7bc4af7bd4f2f6ed
Ammar <ammar@ovasave.com>
2025-07-10 21:00:53 +0400

    setting up mock service worker

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,11 @@
 import { it, expect, describe } from "vitest";
 
 describe("group", () => {
-  it("should", () => {
+  it("should", async () => {
+    const res = await fetch("/categories");
+    const data = await res.json();
+    // console.log("data", data);
+    expect(data.length).toEqual(3);
     expect(1).toBeTruthy();
   });
 });
